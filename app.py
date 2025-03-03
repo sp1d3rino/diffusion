1d487fc8fb4dfb08291dcfb83c78c461592ad4fc
root <fabrizio.rastiell@hotmail.com>
2025-03-03 10:45:30 +0000

    2.1

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 import torch
-from diffusers import StableDiffusion3Pipeline
+from diffusers import StableDiffusionPipeline, DPMSolverMultistepScheduler
 
-pipe = StableDiffusion3Pipeline.from_pretrained("stabilityai/stable-diffusion-3.5-large", torch_dtype=torch.bfloat16)
+model_id = "stabilityai/stable-diffusion-2-1"
+
+# Use the DPMSolverMultistepScheduler (DPM-Solver++) scheduler here instead
+pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
+pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
 pipe = pipe.to("cuda")
 
-image = pipe(
-    "A capybara holding a sign that reads Hello World",
-    num_inference_steps=28,
-    guidance_scale=3.5,
-).images[0]
-image.save("capybara.png")
+prompt = "a photo of an astronaut riding a horse on mars"
+image = pipe(prompt).images[0]
+    
+image.save("astronaut_rides_horse.png")
+
